f8fa36de74d330d28dfbebdc0ed15677ee72cc4a
Gabriel Victor <gabrielvictorweb@outlook.com>
2023-11-27 11:21:27 -0300

    chore: race validations

---
+++ app/Http/Requests/RacingSaveRequest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class RacingSaveRequest extends FormRequest
+{
+    public function rules()
+    {
+        return [
+            'name' => 'required',
+            'date' => 'required|date_format:d-m-Y H:i',
+            'rules' => 'required',
+        ];
+    }
+
+    public function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            'success'   => false,
+            'message'   => 'Validation errors',
+            'data'      => $validator->errors()
+        ], 422));
+    }
+
+    public function messages()
+    {
+        return [
+            'name.required' => 'Name is required',
+            'date.required' => 'Date is required',
+            'rules.required' => 'Rules is required'
+        ];
+    }
+}
